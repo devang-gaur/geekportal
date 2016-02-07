88e1631b35d0caafda889e64e4667d3044246ebf
dg711 <devang.only@gmail.com>
2016-02-07 06:13:51 +0530

    resoting to PDO

--- connect.php
+++ connect.php
@@ -5,17 +5,24 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-$server="ap-cdbr-azure-southeast-b.cloudapp.net";
+$server=/*"localhost";//*/"ap-cdbr-azure-southeast-b.cloudapp.net";//geekportalforumdb
 
-$database="forumdb";
+$database="forumdb";//forumdb
 
-$username="b53a04417753d1";
+$username=/*"root";//*/"b53a04417753d1";//forumdb_admin
 
-$password="c4ba95dc";
+$password=/*"";//*/"c4ba95dc";//Qwerty@123
 
-$connect=mysql_connect($server, $username, $password) or die("Could not connect to the server!");
+//$connect=mysql_connect($server, $username, $password) or die("Could not connect to the server!");
 
-mysql_selectdb($database,$connect) or die("Couldn't connect to the database : $database");
+//mysql_selectdb($database,$connect) or die("Couldn't connect to the database : $database");
 
+
+try{
+    $connect = new PDO("mysql:host=$host;dbname=$db", $username, $password);
+    $connect->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+}catch(Exception $e){
+    die(var_dump($e));
+}
     
 ?>
