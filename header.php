2bfbe90b023e6e66eeb898cd405c7dc1c2bed5bc
dg711 <devang.only@gmail.com>
2016-03-14 05:28:26 +0530

    complete makeover of header

--- header.php
+++ header.php
@@ -1,71 +1,126 @@
 <?php
+require_once 'config.php';
 session_start();
-error_reporting(E_ERROR | E_PARSE); ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www/w3/org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="nl" lang="nl">
+error_reporting(E_ERROR | E_PARSE);
+
+function redirect($url)
+{
+    header('location:'.$url);
+    exit();
+}
+
+?>
+<!DOCTYPE html>
+<html>
     <head>
+        <title>GreetTheGeeks!</title>
+
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
         <meta name="description" content="Your forum for daily tech!" />
         <meta name="keywords" content="" />
-        
+
         <meta property="og:title" content="GreetTheGeeks : your daily hangout spot for tech!"/>
         <meta property="og:type" content="website"/>
         <meta property="og:uri" content="http://localhost:80" />
         <meta property="og:image" content="img/favicon.jpg" />
         <meta property="og:description" content="Discuss your issues.Resolve your queries. " />
-        
-        <title>GreetTheGeeks!</title>
-        
+
+
         <meta name="viewport" content="width=device-width, initial-scale=1">
-		<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
-		<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-		        <!-- <script type="text/javascript" src="js/jquery.js"></script> -->
-	<script type="text/javascript" src="js/accordion.js"></script>
+
+        <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+        <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Lato:100">
+
+
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+        <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+
+        <script type="text/javascript" src="js/accordion.js"></script>
+
+
         <link rel=stylesheet href="css/style.css" />
+        <!-- <link rel=stylesheet href="css/layout_style.css" /> -->
         <link rel=stylesheet href="css/accordionstyle.css" />
-        <link rel=stylesheet href="css/jquery-ui.css" />
-        <!-- <link rel="stylesheet" href="css/jquery.min.css" /> -->
+
     </head>
+
+
     <body>
+
+
+                <div id="header" >
+                <div id="menu" class="container">
+                <div class="row">
+
+                <div class="col-sm-9">
+                <ul class="nav nav-pills">
+                    <li><a class="item" href="index.php">HOME</a></li>
+
+<?php
+                if(''.$_SERVER['SCRIPT_NAME'].'' != addslashes($config['url']."/signup.php") && ''.$_SERVER['SCRIPT_NAME'].''!=addslashes($config['url']."/sign_in.php"))
+                {
+                ?>
+
+                    <?php if($_SESSION['user_level']==1) { ?>
+
+                         <li><a class="item" href="create_category.php">New Category</a></li>
+
+                    <?php } ?>
+
+                         <li><a class="item" href="create_topic.php">New Topic</a></li>
+
+                <?php } ?>
+                        <li><a class="item" href="contact.php">Contact Us</a></li>
+
+                </ul>
+                </div><!-- column ends -->
+                <div class="col-sm-3">
+                <ul class="nav nav-pills userbar">
+
+<?php
+
+                 if($_SESSION['signed_in']==TRUE){ ?>
+
+                            <li class="dropdown">
+                                <button class="dropdown-toggle btn " data-toggle="dropdown">
+                                <img class="dp-thumbnail"  src=<?php echo $_SESSION['user_dp'] ?> />
+                                <?php echo $_SESSION['user_name']; ?><span class="caret"></span>
+                                </button>
+
+                                <ul id="user-choice" class="dropdown-menu">
+                                    <li><a href=<?php echo "changedetails.php?id=".$_SESSION['user_id']; ?>>Change details</a></li>
+                                    <li><a href="sign_out.php">Sign out</a></li>
+                                </ul>
+                            </li>
+
+                <?php
+                        $uid=$_SESSION['user_id'];
+                    }
+
+                    else{
+                        ?>
+
+                        <li><a href="sign_in.php"><div class="btn btn-default">Sign In</div></a></li>
+                        <li><a href="signup.php"><div class="btn btn-default">Sign Up</div></a></li>
+
+                <?php   }   ?>
+                </ul>
+                </div><!-- column ends -->
+            </div><!-- row ends -->
+                </div><!-- menu ends -->
             <div class="wrapper">
-            <div class="header">
-            <div class="menu">
-             <?php
-             //echo '<h1>'.$_SERVER['SCRIPT_NAME'].'</h1><hr />';
-             echo '<h1>GreetTheGeeks</h1><h4>Your daily doze of geeky stuff.</h4><hr />';
-    if(''.$_SERVER['SCRIPT_NAME'].''!=addslashes("/TheForumProject/signup.php") && ''.$_SERVER['SCRIPT_NAME'].''!=addslashes("/TheForumProject/sign_in.php"))
-        { ?>
-   <div id="menubar">
-       <ul>
-            <li><a class="item" href="index.php">HOME</a></li>
-        <?php if($_SESSION['user_level']==1) { ?>    <li><a class="item" href="create_category.php">Create a category</a></li> <?php } ?>
-            <li><a class="item" href="create_topic.php">Create a topic.</a></li>
-            <li><a class="item" href="contact.php">Contact Us</a></li>
-       </ul>
-   </div>
-         <?php if($_SESSION['signed_in']==TRUE){ ?>
-                <div id="userbar">
-                    <ul>
-                        <li>Hello, <?php  echo $_SESSION['user_name']." , "; ?><a class="item" href="sign_out.php">Sign out <?php echo "?" ?></a></li>
-                        
-                        <ul id="user-choice">
-                            <li><a href="changedetails.php?id=<?php echo $uid; ?>">Change details</a></li>
-                            <li><a href="sign_out.php">Sign out</a></li>
-                        </ul>
-                        
-                    </ul>
-                </div>
-         
-   <?php        }
-                else{
-                    ?>
-                <div id="userbar"><a href="sign_in.php">Sign in</a> or <a href="signup.php">Sign up</a> .</div>   
-    <?php            }
-     $uid=$_SESSION['user_id'];
-         }
-         ?>
-                                
-            </div> 
+
+<?php if(''.$_SERVER['SCRIPT_NAME'].'' == addslashes($config['url']."/index.php")){ ?>
+            <!--<div class="container">-->
+            <header>
+
+                <div class="jumbotron">
+
+                <h1>GreetTheGeeks<small>Your daily doze of tech.</small></h1>
+
+                </div><!-- header ends -->
+
+
             </div>
-            <div id="content">
+ <?php } ?>
+            <div id="content" class="container" >
