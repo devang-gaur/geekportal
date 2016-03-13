c4e80d52a7ce022df8f3fba62ebc370205c63c51
dg711 <devang.only@gmail.com>
2016-03-13 23:16:06 +0530

    Security measures!

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+
+$config['server'] = ''; //Enter your server url . eg, localhost
+
+$config['db'] = ''; //Enter your dedicated database for the application
+
+$config['user'] = 'root' ; //Enter the db admin password. 
+
+$config['pass'] = ''; //Enter the db admin password . 
--- connect.php
+++ connect.php
@@ -1,21 +1,18 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-$server="ap-cdbr-azure-east-c.cloudapp.net";
+require_once 'config.php';
 
-$database="DefaultMySQL";
 
-$username="bd4d0820ca880e";
+$server = $config['server'];
 
-$password="ce5fd6a6";
+$database = $config['db'];
+
+$username = $config['user'];
+
+$password = $config['pass'];
 
 $connect=mysql_connect($server, $username, $password) or die("Could not connect to the server!");
 
 mysql_selectdb($database,$connect) or die("Couldn't connect to the database :'.$database");
 
-    
 ?>
