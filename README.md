3d17dea661c119a6736afc4a06cefcb2f5b3c826
dg711 <devang.only@gmail.com>
2016-03-14 06:04:40 +0530

    improved db schema

--- README.md
+++ README.md
@@ -17,11 +17,19 @@ An ideal site to start your day with!
 #####Installation on a local machine :
 
 1.  Import sql/forumdb_schema.sql from phpmyadmin or MySQL command prompt . You have the databases' table structure ready .
-	The admin account ( username : 'admin' , password :'qwertyui' ) and a user account ( username : 'geek' , password : 'abcdefgh' ) would also be created to get you started.
+    The admin account ( username : 'admin' , password :'qwertyui' ) and a user account ( username : 'geek' , password : 'abcdefgh' ) would also be created to get you started.
+    Four categories , namely, SmartPhones, Internet of Things, Tech Startups, Wearables would also be created .
+    Users can start creating topics and post , reply on the created posts and have fun! .
 2.  You can delete the sql folder now if you like .
-3.  Simply copy the whole project into the www directory of your WAMP/LAMP or htdocs incase you use XAMPP .
-4.	Run you WAMP/LAMP/XAMPP and request "localhost/geekportal" on your browser . You made it.
- 
+3.  Simply copy the whole project directory into the www directory of your WAMP/LAMP or htdocs directory incase you use XAMPP .
+4.	Run your WAMP/LAMP/XAMPP and request "localhost/geekportal" on your browser . You made it.
+
+
+#####Admin capabilities (as of now) :
+
+* Creating new categories.
+* Deleting posts.
+* Deleting topics.
 
 >"Don't judge me for the front-end . Rather, **improve** it !."
 
--- sql/forumdb_schema.sql
+++ sql/forumdb_schema.sql
@@ -1,26 +1,6 @@
 
---CREATING ALL THE TABLES STRUCTURES
--- phpMyAdmin SQL Dump
--- version 4.5.2
--- http://www.phpmyadmin.net
---
--- Host: 127.0.0.1
--- Generation Time: Feb 26, 2016 at 04:02 PM
--- Server version: 5.7.9
--- PHP Version: 5.6.16
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `forumdb`
---
 
 -- --------------------------------------------------------
 
@@ -30,26 +10,25 @@ SET time_zone = "+00:00";
 
 DROP TABLE IF EXISTS `categories`;
 CREATE TABLE IF NOT EXISTS `categories` (
-  `cat_id` int(8) NOT NULL AUTO_INCREMENT,
-  `cat_name` varchar(255) NOT NULL,
-  `cat_description` varchar(255) NOT NULL,
-  PRIMARY KEY (`cat_id`),
-  UNIQUE KEY `cat_name_unique` (`cat_name`)
+  `id` int(8) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `last_update` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `cat_name_unique` (`name`)
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
-
 
 --
 -- Dumping data for table `categories`
 --
 
-INSERT INTO `categories` (`cat_id`, `cat_name`, `cat_description`) VALUES
-(1, 'SmartPhones', 'Discussions about different smartphone releases , different platforms and OS. Android , IOS, Blackberry...'),
-(2, 'Internet of Things', 'This category deals with all the discussion related to IoT revolution and  upcoming IoT devices and tech.'),
-(3, 'Tech  Startups', 'Discussions about new tech startups!'),
-(4, 'Wearables', 'This category deals with the all the wearables coming up by different tech- organisations. Eg. Android wear, Apple watch.');
+INSERT INTO `categories` (`id`, `name`, `description`, `last_update`) VALUES
+(1, 'SmartPhones', 'Discussions about different smartphone releases , different platforms and OS. Android , IOS, Blackberry...', '2016-03-08 10:57:44'),
+(2, 'Internet of Things', 'This category deals with all the discussion related to IoT revolution and  upcoming IoT devices and tech.', '2016-03-08 10:57:44'),
+(3, 'Tech Startups', 'Discussions about new tech startups!', '2016-03-08 10:57:44'),
+(4, 'Wearables', 'This category deals with the all the wearables coming up by different tech- organisations. Eg. Android wear, Apple watch.', '2016-03-08 10:57:44');
 
 -- --------------------------------------------------------
-
 
 --
 -- Table structure for table `posts`
@@ -57,18 +36,20 @@ INSERT INTO `categories` (`cat_id`, `cat_name`, `cat_description`) VALUES
 
 DROP TABLE IF EXISTS `posts`;
 CREATE TABLE IF NOT EXISTS `posts` (
-  `post_id` int(8) NOT NULL AUTO_INCREMENT,
-  `post_content` text NOT NULL,
-  `post_date` datetime NOT NULL,
-  `post_topic` int(8) NOT NULL,
-  `post_by` int(8) NOT NULL,
+  `id` int(8) NOT NULL AUTO_INCREMENT,
+  `content` text NOT NULL,
+  `date` datetime NOT NULL,
+  `topic` int(8) NOT NULL,
+  `user` int(8) NOT NULL,
   `upvotes` int(10) UNSIGNED NOT NULL DEFAULT '0' COMMENT 'donotes the no. of upvotes on an answer',
   `downvotes` int(10) UNSIGNED NOT NULL DEFAULT '0' COMMENT 'donotes the no. of downvotes on an answer',
-  PRIMARY KEY (`post_id`),
-  KEY `post_by` (`post_by`),
-  KEY `post_topic` (`post_topic`)
+  PRIMARY KEY (`id`),
+  KEY `post_by` (`user`),
+  KEY `post_topic` (`topic`)
 ) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=latin1;
 
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `replys`
@@ -76,16 +57,17 @@ CREATE TABLE IF NOT EXISTS `posts` (
 
 DROP TABLE IF EXISTS `replys`;
 CREATE TABLE IF NOT EXISTS `replys` (
-  `reply_id` int(11) NOT NULL AUTO_INCREMENT,
-  `reply_content` varchar(300) NOT NULL,
-  `reply_date` date NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `content` varchar(300) NOT NULL,
+  `date` date NOT NULL,
   `post` int(11) NOT NULL,
   `user` int(11) NOT NULL,
-  PRIMARY KEY (`reply_id`),
+  PRIMARY KEY (`id`),
   KEY `post` (`post`),
   KEY `user` (`user`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `topics`
@@ -93,15 +75,16 @@ CREATE TABLE IF NOT EXISTS `replys` (
 
 DROP TABLE IF EXISTS `topics`;
 CREATE TABLE IF NOT EXISTS `topics` (
-  `topic_id` int(8) NOT NULL AUTO_INCREMENT,
-  `topic_subject` text NOT NULL,
-  `topic_date` datetime NOT NULL,
-  `topic_cat` int(8) NOT NULL,
-  `topic_by` int(8) NOT NULL,
-  PRIMARY KEY (`topic_id`),
-  KEY `topic_cat` (`topic_cat`),
-  KEY `topic_by` (`topic_by`)
+  `id` int(8) NOT NULL AUTO_INCREMENT,
+  `subject` text NOT NULL,
+  `date` datetime NOT NULL,
+  `cat` int(8) NOT NULL,
+  `user` int(8) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `topic_cat` (`cat`),
+  KEY `topic_by` (`user`)
 ) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=latin1;
+
 
 
 -- --------------------------------------------------------
@@ -112,21 +95,20 @@ CREATE TABLE IF NOT EXISTS `topics` (
 
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE IF NOT EXISTS `users` (
-  `user_id` int(8) NOT NULL AUTO_INCREMENT,
-  `user_name` varchar(30) NOT NULL,
-  `user_pass` varchar(255) NOT NULL,
-  `user_email` varchar(255) NOT NULL,
-  `user_date` datetime NOT NULL,
-  `user_level` int(8) NOT NULL DEFAULT '1',
-  `user_dp` varchar(500) NOT NULL COMMENT 'stores the file location of the users display pic',
-  PRIMARY KEY (`user_id`),
-  UNIQUE KEY `user_name_unique` (`user_name`),
-  UNIQUE KEY `user_email` (`user_email`),
-  UNIQUE KEY `user_email_2` (`user_email`),
-  UNIQUE KEY `user_email_3` (`user_email`),
-  KEY `user_id` (`user_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
-
+  `id` int(8) NOT NULL AUTO_INCREMENT,
+  `name` varchar(30) NOT NULL,
+  `pass` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `date` datetime NOT NULL,
+  `level` int(8) NOT NULL DEFAULT '1',
+  `dp` varchar(500) NOT NULL COMMENT 'stores the file location of the users display pic',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `user_name_unique` (`name`),
+  UNIQUE KEY `user_email` (`email`),
+  UNIQUE KEY `user_email_2` (`email`),
+  UNIQUE KEY `user_email_3` (`email`),
+  KEY `user_id` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `users`
@@ -144,16 +126,12 @@ INSERT INTO `users` (`user_id`, `user_name`, `user_pass`, `user_email`, `user_da
 -- Constraints for table `posts`
 --
 ALTER TABLE `posts`
-  ADD CONSTRAINT `posts_ibfk_1` FOREIGN KEY (`post_by`) REFERENCES `users` (`user_id`),
-  ADD CONSTRAINT `posts_ibfk_2` FOREIGN KEY (`post_topic`) REFERENCES `topics` (`topic_id`);
+  ADD CONSTRAINT `posts_ibfk_1` FOREIGN KEY (`user`) REFERENCES `users` (`id`),
+  ADD CONSTRAINT `posts_ibfk_2` FOREIGN KEY (`topic`) REFERENCES `topics` (`id`);
 
 --
 -- Constraints for table `topics`
 --
 ALTER TABLE `topics`
-  ADD CONSTRAINT `topics_ibfk_1` FOREIGN KEY (`topic_cat`) REFERENCES `categories` (`cat_id`),
-  ADD CONSTRAINT `topics_ibfk_2` FOREIGN KEY (`topic_by`) REFERENCES `users` (`user_id`);
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+  ADD CONSTRAINT `topics_ibfk_1` FOREIGN KEY (`cat`) REFERENCES `categories` (`id`),
+  ADD CONSTRAINT `topics_ibfk_2` FOREIGN KEY (`user`) REFERENCES `users` (`id`);
