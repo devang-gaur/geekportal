8d1294c45ae8351bc6162f282bd1c35ba1fa25b3
Devang Gaur <devang.only@gmail.com>
2016-03-27 02:43:16 +0530

    Added logging function. logs error to log.txt

--- utility.php
+++ utility.php
@@ -9,10 +9,17 @@ define( 'ADMIN_USER' , 1);
 define( 'USER' , 0);
 
 
-function redirect($url)
-{
+function redirect($url) {
     header('location:'.$url);
     exit();
+}
+
+
+function log_that( $message ) {
+    $current_date = date('d/m/Y  h:i:s');
+    $log_file = fopen('log.txt', 'a');
+    fwrite($log_file, $current_date."\t".$_SERVER['PHP_SELF']."\t\t\"".$message."\"".PHP_EOL);
+    fclose($log_file);
 }
 
 
